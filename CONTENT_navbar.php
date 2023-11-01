dc0f2368ada0b8765e18c53970006bf8ac5dfc0c
anchit-chandran <anchit97123@gmail.com>
2023-11-01 13:56:51 +0000

    conditionally render nav items based on auth
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- CONTENT_navbar.php
+++ CONTENT_navbar.php
@@ -51,19 +51,10 @@ function logged_in()
             <a class="nav-link" href="index.php">Browse</a>
         </li>
         <?php
-
+            if (logged_in()) {
+                include_once('./CONTENT_navbar_logged_in.php');
+            }
         ?>
-        <li class="nav-item mx-1">
-            <a class="nav-link" href="mybids.php">My Bids</a>
-        </li>
-        <li class="nav-item mx-1">
-            <a class="nav-link" href="recommendations.php">Recommended</a>
-        </li>
-        <li class="nav-item mx-1">
-            <a class="nav-link" href="mylistings.php">My Listings</a>
-        </li>
-        <li class="nav-item ml-3">
-            <a class="nav-link btn border-light" href="create_auction.php">+ Create auction</a>
-        </li>
+        
     </ul>
 </nav>
---
+++ CONTENT_navbar_logged_in.php
@@ -0,0 +1,12 @@
+<li class="nav-item mx-1">
+    <a class="nav-link" href="mybids.php">My Bids</a>
+</li>
+<li class="nav-item mx-1">
+    <a class="nav-link" href="recommendations.php">Recommended</a>
+</li>
+<li class="nav-item mx-1">
+    <a class="nav-link" href="mylistings.php">My Listings</a>
+</li>
+<li class="nav-item ml-3">
+    <a class="nav-link btn border-light" href="create_auction.php">+ Create auction</a>
+</li>
