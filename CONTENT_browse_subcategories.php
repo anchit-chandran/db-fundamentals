ab3c33d69216dbd7aeada377e6174fb72da85e38
nghugo <nghugoryan@gmail.com>
2023-11-14 16:57:50 +0000

    remove comments subcategory

--- CONTENT_browse_subcategories.php
+++ CONTENT_browse_subcategories.php
@@ -2,10 +2,6 @@
 
 <?php
 
-// if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-//     print_r( $_GET["category-option"] );
-// }
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     if ($_GET["category-option"] == "all") {
@@ -24,20 +20,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
 }
 
-
-// echo '<select class="form-control" id="subcat">';
-// echo '<option value="fill">' . '</option>';
-// echo '</select>';
-
 ?>
-
-
-
-
-<!-- fetch subcatgories matching the category -->
-<!-- <select class="form-control" id="subcat">
-    <option selected value="all">All subcategories</option>
-    <option value="fill">Fill me in</option>
-    <option value="with">with options</option>
-    <option value="populated">populated from a database?</option>
-</select> -->
