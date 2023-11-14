843547bdf912f3cfef469c9af7e8d7d2974bd046
nghugo <nghugoryan@gmail.com>
2023-11-14 20:51:27 +0000

    finish htmx subcategory query

--- CONTENT_browse_auctionitems_tbody.php
+++ CONTENT_browse_auctionitems_tbody.php
@@ -6,25 +6,28 @@ $finalResult = '';
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $subcategoryId = $_GET["subcategory"];
 
+    
     $productQuery = runQuery("SELECT * 
     FROM Product  
     WHERE subcategoryId = {$subcategoryId};
     ");
 
-    // TODO: Fill in max bid price
-    // TODO: Fill in num bids (bid table)
-    // TODO: Fill in remaining time
-    while ($row = $productQuery->fetch_assoc()) {
+
+// TODO: Fill in remaining time
+while ($row = $productQuery->fetch_assoc()) {
+        
+        $product_id = $row['productId'];
+        $current_price = (array_values(runQuery("SELECT MAX(amount) FROM Bid WHERE productId = " . $product_id)->fetch_assoc())[0]);  // Fetch from Bids
+        $num_bids = (array_values(runQuery("SELECT COUNT(*) FROM Bid WHERE productId = " . $product_id)->fetch_assoc())[0]);  // Fetch from Bids
+        $end_date_str = $row['auctionEndDatetime'];
 
         $finalResult .= "<tr>
-        <th scope='row'>name</th>
-        <td>$row['subCategoryId']</td>
-        <td>£xxxx</td>
-        <td>33</td>
-        <td>24hrs</td>
-      </tr>
-      
-      ";
+        <th scope='row'>{$row['name']}</th>
+        <td>{$row['subcategoryId']}</td>
+        <td>{$current_price}</td>
+        <td>{$num_bids}</td>
+        <td>{$end_date_str}</td>
+      </tr>";
     }
 }
 
