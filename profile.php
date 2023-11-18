c3871fca598d522f616010c759878fea8bce4ba0
Corliss Lam <corlisslam@gmail.com>
2023-11-18 15:07:47 +0000

    working on profile page

--- profile.php
+++ profile.php
@@ -1,5 +1,7 @@
 <?php
+// This profile page is the actual page shown when we type in the website
 $content = 'CONTENT_profile.php';
 $title = 'Profile';
-include('templates/base.php');
+include('templates/base.php'); // base is the like the basics that each page should include, we are including it in profile page
 ?>
+
