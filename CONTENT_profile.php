861bd0f8d5a22e929e73c83703934c217f90a956
Corliss Lam <corlisslam@gmail.com>
2023-11-18 15:06:38 +0000

    profile page content addition

--- CONTENT_profile.php
+++ CONTENT_profile.php
@@ -4,16 +4,46 @@ include_once("CONTENT_header.php");
 include_once("database.php");
 include_once("utilities.php");
 
-
-$getUserID = runQuery("SELECT firstName, lastName FROM User WHERE userId={$_SESSION['userId']}");
-echo $getUserID;
-  
-
 ?>
-
-<div class="row">
+<div class="container">
     <div class="col">
-        <h2>Profile Page</h2>
-            <p></p>
+                <h2>Profile Page</h2>
+                    <p> 
+                        
+                    </p>
+    </div>
+</div>
+
+<div class="container">
+    <h4>Personal Info</h4>
+        <div class="row">
+            <div class="col-6 col-md-4">
+                <?php
+                    echo 'Name:'
+                ?>
+            </div>
+            <div class="col-md-8">
+                <?php
+                    $getUserID = runQuery("SELECT firstName, lastName FROM User WHERE userId={$_SESSION['userId']}");
+                    echo $getUserID;
+                ?>
+            </div>
+        
+        </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-6 col-md-4">
+            <?php
+                echo 'Name:'
+            ?>
+        </div>
+        <div class="col-md-8">
+            <?php
+                $getAddressOne = runQuery("SELECT address_1 FROM Address WHERE userId={$_SESSION['userId']}");
+                echo $getAddressOne;
+            ?>
+        </div>
+       
     </div>
 </div>
