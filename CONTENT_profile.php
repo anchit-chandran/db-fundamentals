1660c8830e517a528909cea949cf0785879e7641
Corliss Lam <corlisslam@gmail.com>
2023-11-18 15:29:19 +0000

    working on profile page

--- CONTENT_profile.php
+++ CONTENT_profile.php
@@ -1,11 +1,19 @@
 <?php
+// This is were you make the content of the profile page, but it is linked to the profile.php page
 include_once("CONTENT_header.php");
 include_once("database.php");
 include_once("utilities.php");
+
+
+$getUserID = runQuery("SELECT firstName, lastName FROM User WHERE userId={$_SESSION['userId']}");
+echo $getUserID;
+  
+
 ?>
 
 <div class="row">
     <div class="col">
         <h2>Profile Page</h2>
+            <p></p>
     </div>
 </div>
