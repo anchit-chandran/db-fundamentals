87ad91a62e58c6530781cdb0ef224bf0ac925f7a
anchit-chandran <anchit97123@gmail.com>
2023-11-20 18:47:46 +0000

    attempt to set base timezone in all scripts
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- templates/base.php
+++ templates/base.php
@@ -1,6 +1,7 @@
 <?php
 include_once('setup.php');
 include_once("utilities.php");
+date_default_timezone_set('Europe/London');
 ?>
 
 
--- templates/setup.php
+++ templates/setup.php
@@ -1,3 +1,5 @@
 <?php
 include_once('session_start.php');
+
+date_default_timezone_set('Europe/London');
 ?>
