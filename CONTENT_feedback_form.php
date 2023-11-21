70b7d8eb75747c002f6f9cd0396d02949056b1a6
nghugo <nghugoryan@gmail.com>
2023-11-21 18:17:20 +0000

    add sql injection prone feedback form

--- CONTENT_feedback_form.php
+++ CONTENT_feedback_form.php
@@ -6,183 +6,46 @@ $product_name = runQuery("SELECT name FROM Product WHERE productId = {$product_i
 <div class="container">
   <div style="max-width: 800px; margin: 10px auto">
     <h2 class="my-3">Create Feedback</h2>
-    <p>Product: <?php echo $product_name; ?></p>
+    <p>You were the highest bidder for this product: 
+      <span class="fw-bold">
+      <?php echo $product_name; ?>
+      </span>
+    </p>
     
     <div class="card">
       <div class="card-body">
         <form method="post" action="create_feedback.php" id="createFeedbackForm" enctype="multipart/form-data">
 
         <div>
-        <label for="feedback-rating"" class="col-sm col-form-label text-right">Rating (required)</label>
+        <label for="rating"" class="col-sm col-form-label text-right">Rating (required)</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="feedback-rating" id="inlineRadio1" value="option1" required>
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio1" value="1" required>
           <label class="form-check-label" for="inlineRadio1">1</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="feedback-rating" id="inlineRadio2" value="option2">
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio2" value="2">
           <label class="form-check-label" for="inlineRadio2">2</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="feedback-rating" id="inlineRadio3" value="option3">
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio3" value="3">
           <label class="form-check-label" for="inlineRadio3">3</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="feedback-rating" id="inlineRadio4" value="option4">
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio4" value="4">
           <label class="form-check-label" for="inlineRadio4">4</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="feedback-rating" id="inlineRadio5" value="option5">
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio5" value="5">
           <label class="form-check-label" for="inlineRadio5">5</label>
         </div>
     
-          <div><label for="feedback-message"" class=" col-sm col-form-label text-right">Message (max 2000 characters)</label></div>
-          <textarea name="feedback-message" class="form-control mb-2" rows="6" maxlength="2000"></textarea>
+          <div><label for="comment"" class=" col-sm col-form-label text-right">Message (max 2000 characters)</label></div>
+          <textarea name="comment" class="form-control mb-2" rows="6" maxlength="2000"></textarea>
+          <input name="user_id" type="hidden" value=<?php echo $_SESSION['userId']; ?>>
+          <input name="product_id" type="hidden" value=<?php echo $product_id; ?>>
           <button type="submit" class="btn btn-primary form-control">Send/Update Feedback</button>
         </form>
-
-        <!-- <form method="post" action="create_auction_result.php" id="createAuctionForm" enctype="multipart/form-data">
-        <div class="form-group row">
-          <label for="auctionTitle" class="col-sm-2 col-form-label text-right">Title of auction</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="auctionTitle" name="auctionTitle" placeholder="e.g. Black mountain bike">
-            <small id="titleHelp" class="form-text text-muted"><span class="text-danger" id="titleDanger">* Required.</span> Title of the item you're selling, which will display in listings. Max: 500 characters</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionDetails" class="col-sm-2 col-form-label text-right">Details</label>
-          <div class="col-sm-10">
-            <textarea class="form-control" id="auctionDetails" name="auctionDetails" rows="4"></textarea>
-            <small id="detailsHelp" class="form-text text-muted"><span class="text-danger" id="detailsDanger">* Required.</span> A short description of the item you're selling, which will display in listings. Max: 1000 characters</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionCategory" class="col-sm-2 col-form-label text-right">Category</label>
-          <div class="col-sm-10">
-            <select class="form-control" id="auctionCategory" name="auctionCategory">
-            <option value="">Choose...</option>
-            <?php
-            foreach ($categories as $category) {
-              echo "<option value=\"{$category['categoryId']}\">{$category['categoryName']}</option>";
-            }
-            ?>
-            </select>
-            <small id="categoryHelp" class="form-text text-muted"><span class="text-danger" id="categoryDanger">* Required.</span> Select a category for this item.</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionSubCategory" class="col-sm-2 col-form-label text-right">Subcategory</label>
-          <div class="col-sm-10">
-            <select class="form-control" id="auctionSubCategory" name="auctionSubCategory">
-              
-              <script>
-                const categoryChoice = document.getElementById("auctionCategory");
-                categoryChoice.addEventListener("change", () => {
-                  const selectedCategory = categoryChoice.value;
-                  fetchSubcategory(selectedCategory);
-                })
-                function fetchSubcategory(categoryId) {
-                  const subcategoryChoice = document.getElementById("auctionSubCategory");
-                  subcategoryChoice.innerHTML = "";
-                  if (categoryId !== "") {
-                    
-                    var subcategories = <?php echo json_encode($subcategories); ?>;
-                    for (var i = 0; i < subcategories.length; i++) {
-                          if (subcategories[i].categoryId === categoryId) {
-                            var option = document.createElement("option");
-                            option.text = subcategories[i].subCategoryName;
-                            option.value = subcategories[i].subCategoryId;
-                            subcategoryChoice.add(option)
-                          }
-                          
-                        }
-
-                  } 
-                    else {
-                        var option = document.createElement("option");
-                          option.text = "Choose category first";
-                          option.value = "";
-                          subcategoryChoice.add(option);
-                  }
-                }
-                fetchSubcategory("")
-              </script>
-            </select>
-            <small class="form-text text-muted"></small>
-            <small id="subcategoryHelp" class="form-text text-muted">Select a subcategory for this item.</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionCondition" class="col-sm-2 col-form-label text-right">Item Condition</label>
-          <div class="col-sm-10">
-            <select class="form-control" id="auctionCondition" name="auctionCondition">
-              <option value="">Choose...</option>
-              <option value="Brand New">Brand New</option>
-              <option value="Slightly Used">Slightly Used</option>
-              <option value="Used">Used</option>
-            </select>
-            <small id="conditionHelp" class="form-text text-muted"><span class="text-danger" id="conditionDanger">* Required.</span> Select a condition for this item.</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionImage" class="col-sm-2 col-form-label text-right">Item Image</label>
-          <div class="col-sm-10">
-          <input type="file" id="auctionImage" name="auctionImage" accept=".jpg, .jpeg, .png,">
-          <button type="button" id="removeImage" class="d-none" onclick="clearImage()">Remove</button>
-          <small id="imageHelp" class="form-text text-muted">Upload image for this listing (.jpg, .jpeg, .png) max: 5 MB</small>
-          </div>
-          
-          
-        </div>
-        <div class="form-group row">
-          <label for="auctionStartPrice" class="col-sm-2 col-form-label text-right">Starting price</label>
-          <div class="col-sm-10">
-	        <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text">£</span>
-              </div>
-              <input type="number" class="form-control" id="auctionStartPrice" name="auctionStartPrice" step="any">
-            </div>
-            <small id="startBidHelp" class="form-text text-muted"><span class="text-danger" id="startPriceDanger">* Required.</span> Initial bid amount. Amount must be a valid number and greater than or equal to 0</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionReservePrice" class="col-sm-2 col-form-label text-right" >Reserve price</label>
-          <div class="col-sm-10">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text">£</span>
-              </div>
-              <input type="number" class="form-control" id="auctionReservePrice" name="auctionReservePrice" step="any">
-            </div>
-            <small id="reservePriceHelp" class="form-text text-muted">Optional. Auctions that end below this price will not go through. This value is not displayed in the auction listing.</small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="auctionEndDate" class="col-sm-2 col-form-label text-right">Start date</label>
-          <div class="col-sm-10">
-            <div class="input-group"> 
-            <div class="input-group-prepend">
-            <button type="button" id="startNow" class="" onclick="currDate()">Start Now</button>
-              </div>
-            <input type="datetime-local" class="form-control" id="auctionStartDate" name="auctionStartDate">
-            </div>
-            <small id="startDateHelp" class="form-text text-muted"><span class="text-danger" id="startDateDanger">* Required.</span> Day for the auction to end. Day must be in the future or now</small>
-            
-            
-          </div>
-          
-
-        </div>
-        <div class="form-group row">
-          <label for="auctionEndDate" class="col-sm-2 col-form-label text-right">End date</label>
-          <div class="col-sm-10">
-            <input type="datetime-local" class="form-control" id="auctionEndDate" name="auctionEndDate">
-            <small id="endDateHelp" class="form-text text-muted"><span class="text-danger" id="endDateDanger">* Required.</span> Day for the auction to end. Date must be after start date</small>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary form-control">Create Auction</button>
-      </form> -->
       </div>
     </div>
   </div>
--- create_feedback.php
+++ create_feedback.php
@@ -16,77 +16,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         session_start();
     }
 
-    // $product_id = $_POST["product_id"];
-    // $bid_amount = floatval($_POST["bid_amount"]);
-    // $user_id = $_SESSION["userId"];
+    $rating = $_POST["rating"];
+    $user_id = $_POST["user_id"];
+    $comment = $_POST["comment"];
+    $comment = !empty($comment) ? "'$comment'" : "NULL";  // set to "NULL" string if empty
+    $product_id = $_POST["product_id"];
+
+    // Clear previous feedback of product
+    runQuery(
+        "DELETE
+        FROM Feedback
+        WHERE productId = {$product_id} AND userId = {$user_id}"
+    );
     
-    // // Fetch max bid of product to compare against current bid
-    // $highest_bid = floatval(runQuery("SELECT MAX(amount) FROM Bid WHERE productId = " . $product_id)->fetch_assoc()[0]);
-    // $highest_bid_user = runQuery("SELECT userId FROM Bid WHERE productId = {$product_id} ORDER BY amount DESC LIMIT 1")->fetch_assoc()['userId'];
+    runQuery(
+        "INSERT INTO Feedback (comment, rating, productId, userId) 
+        VALUES ({$comment}, {$rating}, {$product_id}, {$user_id})"
+    );
 
-    // $starting_price = runQuery("SELECT startPrice FROM Product WHERE productId = " . $product_id)->fetch_assoc()[0];
-    // $end_date_str = runQuery("SELECT auctionEndDatetime FROM Product WHERE productId = " . $product_id)->fetch_assoc()[0];
-    // $now = new DateTime();
-    // $end_date = datetime::createFromFormat('Y-m-d H:i:s', $end_date_str);
+    $test_feedback = runQuery(
+        "SELECT *
+        FROM Feedback
+        WHERE productId = {$product_id} AND userId = {$user_id}"
+    )->fetch_assoc();
 
-
-    // if ($end_date > $now) {
-    //     $_SESSION["flash"] = ["type" => "warning", "message" => "Failed to add bid. The auction has ended."];
-    //     header("Location: listing.php?productId={$product_id}");
-    // } else if ($bid_amount <= $starting_price) {
-
-    //     $_SESSION["flash"] = ["type" => "warning", "message" => "Failed to add bid. Your bid needs to be greater than the starting price."];
-    //     header("Location: listing.php?productId={$product_id}");
-    // } else if ($bid_amount <= $highest_bid) {
-    //     $_SESSION["flash"] = ["type" => "warning", "message" => "Failed to add bid. Your bid needs to be greater than the highest bid."];
-    //     header("Location: listing.php?productId={$product_id}");
-    // } else if ($highest_bid_user == $user_id) {
-    //     $_SESSION["flash"] = ["type" => "warning", "message" => "Failed to add bid. You cannot bid against yourself as the highest bidder. "];
-    //     header("Location: listing.php?productId={$product_id}");
-    // } else {
-    //     $add_bid = "INSERT INTO Bid (amount, productId, userId)
-    //         VALUES ({strval($bid_amount)}, {$product_id}, {$user_id});";
-        
-        
-    //     runQuery($add_bid);
-    //     $_SESSION["flash"] = ["type" => "success", "message" => "Bid added successfully"];
-
-    //     // SEND EMAIL TO BEATEN BIDDER
-    //     $beatenUserId = runQuery("
-    //         SELECT userId 
-    //         FROM Bid
-    //         WHERE productId={$product_id} AND userId != {$user_id}
-    //         ORDER BY amount DESC
-    //         LIMIT 1
-    //     ")->fetch_assoc();
-
-
-    //     // ENSURE THERE IS A PREVIOUS USER
-    //     if ($beatenUserId) {
-    //         $beatenUserEmail = runQuery("SELECT email
-    //         FROM user
-    //         WHERE userId = {$beatenUserId['userId']}")->fetch_assoc()['email'];
-
-    //         // SEND EMAIL TO USER
-    //         // IF CONFIG.EMAIL_SENDING == True
-    //         if ($EMAIL_SENDING) {
-
-    //             //  Product details
-    //             $productDetails = runQuery("SELECT * FROM Product WHERE productId = {$product_id}")->fetch_assoc();
-
-    //             $productLink = "https://localhost/db-fundamentals/listing.php?productId={$product_id}";
-
-    //             $to = $beatenUserEmail;
-    //             $subject = "You've been outbid!";
-    //             $message = "Oh no, you've been outbid for {$productDetails['name']}!\n\nIf you'd like to place another bid on this product, please go to: {$productLink} \n\nThanks,\nThe Db-Friends Team";
-    //             $header = "From: anchit97123@gmail.com";
-    //             if (!mail($to, $subject, $message, $header)) {
-    //                 $_SESSION["flash"] = ["type" => "success", "message" => "Bid added successfully" . "Failed to send email to {$beatenUserEmail}"];
-    //             }
-    //         }
-    //     }
-
-        header("Location: index.php");
-        // header("Location: feedback.php");
-    // }
+    // TODO: remove print_r and redirect to product (but first display the feedback on the product page)
+    print_r($test_feedback);
+    // header("Location: listing.php?productId={$product_id}");
 }
