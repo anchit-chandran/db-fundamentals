572446859504aad16cc25bbee8062147bba51377
nghugo <nghugoryan@gmail.com>
2023-11-21 22:15:31 +0000

    add clickable href and bold

--- CONTENT_mylistings.php
+++ CONTENT_mylistings.php
@@ -39,7 +39,7 @@
                         $category_id = (array_values(runQuery("SELECT categoryId FROM SubCategory WHERE subCategoryId = " . $row['subcategoryId'])->fetch_assoc())[0]);
                         $category_name = (array_values(runQuery("SELECT categoryName FROM Category WHERE categoryId = " . $category_id)->fetch_assoc())[0]);
                         echo "<tr>
-                            <td>{$row['name']}</td>
+                            <th><a href='listing.php?productId={$row['productId']}'>{$row['name']}</a></th>
                             <td>{$row['startPrice']}</td>
                             <td>{$row['reservePrice']}</td>
                             <td>{$highest_bid}</td>
