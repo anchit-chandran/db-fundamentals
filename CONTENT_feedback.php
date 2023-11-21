dfe467044f169e33b743f2061fda539e27f1ab3a
nghugo <nghugoryan@gmail.com>
2023-11-21 18:50:19 +0000

    add check against bid lower than reserve

--- CONTENT_feedback.php
+++ CONTENT_feedback.php
@@ -5,6 +5,7 @@ include_once 'database.php';
 $product_id_in_param = false;
 $product_found = false;
 $auction_ended = false;
+$highest_bid_at_least_reserve = false;
 $user_logged_in = false;
 $user_matches_highest_bidder = false;
 
@@ -26,6 +27,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if ($product_found) {
         // Fetch max bid of product to compare against current bid
         $highest_bid = floatval(runQuery("SELECT MAX(amount) FROM Bid WHERE productId = " . $product_id)->fetch_assoc()["MAX(amount)"]);
+        if ($highest_bid >= floatval($result['reservePrice']) && $highest_bid >= floatval($result['startPrice'])) {
+            $highest_bid_at_least_reserve = true;
+        }
         $highest_bid_user_id = runQuery("SELECT userId FROM Bid WHERE productId = {$product_id} ORDER BY amount DESC LIMIT 1")->fetch_assoc();
         if ($highest_bid_user_id != null) {
             $highest_bid_user_id = $highest_bid_user_id['userId'];
@@ -47,6 +51,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         echo "<div class='alert alert-warning mt-4 mb-0' role='alert'>You must log in to access the feedback page.</div>";
     } else if (!$auction_ended) {
         echo "<div class='alert alert-warning mt-4 mb-0' role='alert'>The auction for this product has not yet ended, so you cannot access the feedback page.</div>";
+    } else if (!$highest_bid_at_least_reserve) {
+        echo "<div class='alert alert-warning mt-4 mb-0' role='alert'>The product did not sell as bids were too low, so you cannot access the feedback page.</div>";
     } else if (!$user_matches_highest_bidder) {
         echo "<div class='alert alert-warning mt-4 mb-0' role='alert'>You are not the highest bidder for this product, so you cannot access the feedback page.</div>";
     } else {
