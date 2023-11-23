621232d569cefbb4698243b809a02a7cdb36baaf
anchit-chandran <anchit97123@gmail.com>
2023-11-23 15:15:00 +0000

    adds links to public profile page from other pages
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- CONTENT_profile.php
+++ CONTENT_profile.php
@@ -8,6 +8,45 @@ $userId = $_GET['userId'];
 
 $userDetails = runQuery("SELECT * FROM User WHERE userId = {$userId}")->fetch_assoc();
 
+$products = runQuery("SELECT
+P.productId,
+P.name,
+P.description,
+P.auctionStartDatetime,
+P.auctionEndDatetime,
+P.reservePrice,
+P.startPrice,
+P.createdAt,
+P.updatedAt,
+P.image,
+P.state,
+P.userId,
+P.subcategoryId, 
+MAX(B.amount) AS highestBidAmount
+FROM Product as P
+LEFT JOIN 
+    bid AS B ON P.productId = B.productId
+WHERE 
+  P.auctionEndDatetime > NOW()
+  AND P.userId = {$userId}
+GROUP BY
+    P.productId,
+    P.name,
+    P.description,
+    P.auctionStartDatetime,
+    P.auctionEndDatetime,
+    P.reservePrice,
+    P.startPrice,
+    P.createdAt,
+    P.updatedAt,
+    P.image,
+    P.state,
+    P.userId,
+    P.subcategoryId
+
+  ORDER BY
+    highestBidAmount DESC");
+
 ?>
 
 <form class="row" method='POST'>
@@ -28,6 +67,70 @@ $userDetails = runQuery("SELECT * FROM User WHERE userId = {$userId}")->fetch_as
             <div class="d-flex justify-content-between align-items-center mb-3">
                 <h4 class="text-right">Auctions</h4>
             </div>
+
+            <?php
+            if (mysqli_num_rows($products) == 0) {
+                echo "<p>This user has not made any auctions yet.</p>";
+            } else {
+                echo "<table class='table table-hover' id='auction_items_table'>
+                        <thead>
+                            <tr>
+                                <th scope='col'>Auction Image</th>
+                                <th scope='col'>Name</th>
+                                <th scope='col'>Description</th>
+                                <th scope='col'>Highest bid <?php echo '⬇️' ?></th>
+                                <th scope='col'>Number of bids</th>
+                                <th scope='col'>Remaining time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        ";
+
+                while ($row = $products->fetch_assoc()) {
+
+                    // GET N BIDS
+                    $num_bids = (array_values(runQuery("SELECT COUNT(*) FROM Bid WHERE productId = " . $row['productId'])->fetch_assoc())[0]);
+
+                    // GET TIME REMAINING
+                    $end_date_str = $row['auctionEndDatetime'];
+                    $now = new DateTime();
+                    $end_date = datetime::createFromFormat('Y-m-d H:i:s', $end_date_str);
+                    if ($now > $end_date) {
+                        $time_remaining = 'This auction has ended';
+                    } else {
+                        // Get interval:
+                        $time_to_end = date_diff($now, $end_date);
+                        $time_remaining = display_time_remaining($time_to_end);
+                    }
+
+                    //  RENDER HIGHEST BID AMOUNT
+                    if ($row['highestBidAmount'] > 0) {
+                        $highestBidAmount = "£{$row['highestBidAmount']}";
+                    } else {
+                        $highestBidAmount = "No bids!";
+                    }
+
+                    if ($row["image"] != null) {
+                        $image = $row["image"];
+                        $base64image = base64_encode($image);
+                        $imageField = "<div class='d-flex align-itemes-center justify-content-center' width='100' height='100'><img src='data:image/jpeg;base64," . $base64image . "' alt='Blob Image' style='object-fit:contain' width='100' height='100'></div>";
+                    } else {
+                        $imageField = "<p><span class='fw-bold'>No image uploaded with this listing</span></p>";
+                    }
+                    echo "<tr>
+              <td class='col-1'>$imageField</td>
+                  <th scope='row'><a href='listing.php?productId={$row['productId']}'>{$row['name']}</a></th>
+                  <td>{$row['description']}</td>
+                  <td>{$highestBidAmount}</td>
+                  <td>{$num_bids}</td>
+                  <td>{$time_remaining}</td>
+                  </tr>";
+                }
+
+                echo "</tbody>
+                    </table>";
+            }
+            ?>
 
         </div>
 
