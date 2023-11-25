ec986e3d5ed80fd23ad9bce24c44b5aa44ea2f17
anchit-chandran <anchit97123@gmail.com>
2023-11-25 12:20:01 +0000

    adds fw bold to public auction
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- CONTENT_profile.php
+++ CONTENT_profile.php
@@ -151,7 +151,7 @@ GROUP BY
                     $productLink = "listing.php?productId={$row['productId']}";
                     echo "<tr data-url='{$productLink}' class='clickable_tr'>
               <td class='col-1'>$imageField</td>
-                  <td><a href='{$productLink}'>{$row['name']}</a></th>
+                  <td class='fw-bold'><a href='{$productLink}'>{$row['name']}</a></th>
                   <td>{$row['description']}</td>
                   <td>{$highestBidAmount}</td>
                   <td>{$num_bids}</td>
