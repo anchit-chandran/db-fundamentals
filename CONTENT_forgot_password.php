0a5012a7da589d5a2351551355d9ad588814c8ae
anchit-chandran <anchit97123@gmail.com>
2023-11-29 10:50:20 +0000

    fix bid seed -> john bid on own listing
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- CONTENT_forgot_password.php
+++ CONTENT_forgot_password.php
@@ -6,28 +6,26 @@ include_once("utilities.php");
 
 <?php
 
-    $email_error = '';
+$email_error = '';
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $email = $_POST['email'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $email = $_POST['email'];
 
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $to = $email;
-            $subject = "Reset Password";
-            $token = password_hash($email, PASSWORD_DEFAULT);
-            $confirmation_link = "http://localhost/db-fundamentals/reset_password.php?token={$token}&email={$email}";
-            $message = "Hi,\n\nPlease click here to reset your password:\n\n{$confirmation_link}\n\nThanks,\nThe Db-Friends Team";
-            $header = "From: anchit97123@gmail.com";
-            if (mail($to, $subject, $message, $header)) {
-                header("Location:index.php");
-            } else {
-                echo "Sorry, failed while sending mail!";
-            }
-        }
-        else {
-            $email_error = '<ul><li><p>Please enter a valid email.</p></li></ul>';
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $to = $email;
+        $subject = "Reset Password";
+        $token = password_hash($email, PASSWORD_DEFAULT);
+        $confirmation_link = "http://localhost/db-fundamentals/reset_password.php?token={$token}&email={$email}";
+        $message = "Hi,\n\nPlease click here to reset your password:\n\n{$confirmation_link}\n\nThanks,\nThe Db-Friends Team";
+        $header = "From: anchit97123@gmail.com";
+        if (mail($to, $subject, $message, $header)) {
+            $_SESSION["flash"] = ["type" => "success", "message" => "If that email exists, you will be emailed a link to reset your password."];
+            header("Location:index.php");
+        } else {
+            echo "Sorry, failed while sending mail!";
         }
     }
+}
 ?>
 
 <div class="row mt-5 justify-content-center signin_row">
